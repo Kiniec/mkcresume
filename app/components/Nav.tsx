66579f967cfd2b0d60d8b450db5e957e2761985a
Mar <mar.kiniec@gmail.com>
2025-07-18 12:02:06 -0500

    first modifications

---
+++ app/components/Nav.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+//import { CircleChevronLeft } from "lucide-react";
+import Link from "next/link";
+import React, { useEffect, useRef, useState } from "react";
+import ThemeToggle from "./Theme-Toggle";
+
+
+ const Nav  = () => { //React.FC
+	const ref = useRef<HTMLElement>(null);
+	const [isIntersecting, setIntersecting] = useState(true);
+
+	useEffect(() => {
+		if (!ref.current) return;
+		const observer = new IntersectionObserver(([entry]) =>
+			setIntersecting(entry.isIntersecting),
+		);
+
+		observer.observe(ref.current);
+		return () => observer.disconnect();
+	}, []);
+
+	return (
+		<header ref={ref}>
+		
+			<div
+				className={`fixed inset-x-0 top-0 z-50 backdrop-blur  duration-200 border-b  ${
+					isIntersecting
+						? "bg-zinc-900/0 border-transparent"
+						: "bg-zinc-900/500  border-zinc-800 "
+				}`}
+			>
+				
+				
+				<div className="container   flex flex-row-reverse  justify-center  py-5 mx-auto mx-w-md">
+					
+					<div className="flex justify-between gap-3 sm:gap-7">
+						<Link
+						href="/" /* for return to home page*/
+						className="duration-200   dark:text-zinc-400 dark:hover:text-zinc-100"
+					>
+						{/* <CircleChevronLeft className="w-6 h-6 " />  */}
+						
+
+						
+					</Link>
+						<Link
+							href="/about"
+							className="duration-200  dark:text-zinc-400 dark:hover:text-zinc-100  "
+						>
+							About
+						</Link>
+						<Link
+							href="/profile"
+							className="duration-200  dark:text-zinc-400 dark:hover:text-zinc-100"
+						>
+							Profile 
+						</Link>
+						<Link
+							href="/projects"
+							className="duration-200   dark:text-zinc-400 dark:hover:text-zinc-100"
+						>
+							Projects
+						</Link>
+						<Link
+							href="/contact"
+							className="duration-200  dark:text-zinc-400 dark:hover:text-zinc-100"
+						>
+							Contact
+						</Link>
+						{/* <Link */}
+							{/* href="/blog" */}
+							{/* className="duration-200 text-zinc-400 hover:text-zinc-100" */}
+						{/* > */}
+							{/* Blog */}
+						{/* </Link> */}
+
+						<div className=" ">
+						    <ThemeToggle/>  
+					</div>
+						
+					</div>
+
+					
+					
+					
+					
+				</div>
+				
+			</div>
+		</header>
+	);
+};
+export default Nav
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,64 @@
-import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Particles from "./components/Particles";
+import ThemeToggle from "./components/Theme-Toggle";
 
+const navigation = [
+  { name: "About", href: "/about" },
+  { name: "Profile", href: "/profile" },
+  { name: "Projects", href: "/projects" },
+  { name: "Contact", href: "/contact" },
+  // { name: "Blog", href: "/blog" },
+];
+
+
+ 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden   ">   {/* dark:bg-gradient-to-tl from-zinc-900/0  via-zinc-900 dark:to-zinc-900/0 */}
+             {/*  */}
+      <nav className="my-16 animate-fade-in duration-1000">
+        <ul className="flex items-center justify-center gap-4">
+          
+          {navigation.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="text-lg font-poppins  "
+            >
+              {item.name}
+            </Link>
+          ))}
+           <ThemeToggle/>  
+        </ul>
+        
+      </nav>
+          
+        <div className="  w-screen h-px  duration-1000 animate-collapsible-down bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" /> {/*Line above name */}
+             <Particles
+                      className="absolute inset-0 -z-10 zoom-in-100  "
+                      quantity={2000}
+                      /> 
+              <h1 className="  py-5 px-5 mask-b-to-100%  text-5xl  dark:text-zinc-200  cursor-default  sm:text-6xl md:text-9xl   "> {/* text-shadow-transparent font-display bg-white  text-edge-outline whitespace-wrap bg-clip-text*/}
+                  Mar Kiniec Carlisle
+              </h1>
+               
+          <div className=" w-screen h-px duration-1000   animate-accordion-up bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" /> {/* Line below name*/}
+         
+         <div className="my-16 text-center duration-1000 fade-left">
+          <h2 className="text-lg    font-poppins dark:text-zinc-400 dark:hover:text-white ">
+           Life is CI/CD. Always continue to build and improve!
+          </h2>
+          </div>
+      
+         
+      
+  
+    
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
+    
   );
+ 
 }
